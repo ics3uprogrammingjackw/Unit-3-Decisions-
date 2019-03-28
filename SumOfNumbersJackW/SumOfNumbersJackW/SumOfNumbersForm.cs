4ec09aa46ee2cd088a224ac64938d41420887187
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-03-28 12:17:10 -0400

    Finished sum of numbers code

--- SumOfNumbersJackW/SumOfNumbersJackW/SumOfNumbersForm.cs
+++ SumOfNumbersJackW/SumOfNumbersJackW/SumOfNumbersForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Jack Wright
+ * Created on: 28-03-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #23 - Sum of Numbers
+ * This program calculates the sum of a user inputted number
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,21 +25,26 @@ namespace SumOfNumbersJackW
             InitializeComponent();
         }
 
+        //Declaring variables
         Double userNumber;
         Double sumCounter;
-        Double sumAnswer;
+        Double sumAnswer = 0;
 
         private void txtSum_TextChanged(object sender, EventArgs e)
         {
-            userNumber = Convert.ToDouble(txtSum);
+            //Gets user input
+            userNumber = Convert.ToDouble(txtSum.Text);
 
-            for (sumCounter = 1; sumCounter <= userNumber;)
+            for (sumCounter = 1; sumCounter <= userNumber; sumCounter++)
             {
-
-
+                //Calulates sum of number
+                sumAnswer = sumAnswer + sumCounter;
 
 
             }
+
+            //Displays answer
+            lblSum.Text = Convert.ToString(sumAnswer);
 
 
 
