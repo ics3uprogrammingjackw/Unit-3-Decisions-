24ca2d7531af461325d5f93dd60da8a331cf86bd
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-04-02 12:59:09 -0400

    Unicode added

---
+++ UnicodeJackW/UnicodeJackW/UnicodeForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace UnicodeJackW
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstUnicode = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstUnicode
+            // 
+            this.lstUnicode.FormattingEnabled = true;
+            this.lstUnicode.Location = new System.Drawing.Point(113, 102);
+            this.lstUnicode.Name = "lstUnicode";
+            this.lstUnicode.Size = new System.Drawing.Size(139, 186);
+            this.lstUnicode.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(143, 40);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 405);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstUnicode);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode by Jack W";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstUnicode;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ UnicodeJackW/UnicodeJackW/UnicodeForm.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by: Jack Wright
+ * Created on: 02-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #24 - Unicode
+ * This program converts capital letter A-Z into their hexadecimal value and lists the letter next to its value.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeJackW
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Declaring variables
+            string uniLetter;
+            int counter = 65;
+
+            //Clears items on the list
+            lstUnicode.Items.Clear();
+
+            for(counter = 65; counter <= 90; counter++)
+            {
+                //Converts value into its respective letter
+                uniLetter = Char.ConvertFromUtf32(counter);
+
+                //Displays outcome in the listbox
+                lstUnicode.Items.Add(uniLetter + "->" + counter);
+
+            }
+        }
+    }
+}
