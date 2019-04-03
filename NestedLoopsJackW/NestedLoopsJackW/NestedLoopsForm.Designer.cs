6149aa4352d0b5b5e68be1c5014226592304978e
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-04-03 12:50:57 -0400

    Nested loops added and finished

---
+++ NestedLoopsJackW/NestedLoopsJackW/NestedLoopsForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NestedLoopsJackW
+{
+    partial class frmNestedLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAlphabet = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstAlphabet
+            // 
+            this.lstAlphabet.FormattingEnabled = true;
+            this.lstAlphabet.Location = new System.Drawing.Point(189, 102);
+            this.lstAlphabet.Name = "lstAlphabet";
+            this.lstAlphabet.Size = new System.Drawing.Size(163, 199);
+            this.lstAlphabet.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(225, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 361);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstAlphabet);
+            this.Name = "frmNestedLoops";
+            this.Text = "Nested Loops by Jack W";
+            this.Load += new System.EventHandler(this.frmNestedLoops_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstAlphabet;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ NestedLoopsJackW/NestedLoopsJackW/NestedLoopsForm.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Created by: Jack Wright
+ * Created on: 03-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Alphabet Nested Loops
+ * This program uses nested loops to show conversions of capital letters to lowercase letters
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedLoopsJackW
+{
+    public partial class frmNestedLoops : Form
+    {
+        public frmNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNestedLoops_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Declaring variables
+            string capital;
+            string lowercase;
+            int counter1;
+            int counter2;
+
+            //Clears items on the listbox
+            lstAlphabet.Items.Clear();
+
+            for(counter1 = 65; counter1 <= 90; counter1++)
+            {
+                //Convert counter to its respective letter in unicode
+                capital = Char.ConvertFromUtf32(counter1);
+
+                
+
+                for (counter2 = 97; counter2 <= 122; counter2++)
+                {
+                    ////Convert counter to its respective letter in unicode
+                    lowercase = Char.ConvertFromUtf32(counter2);
+
+                    //Displays converted letters in the listbox
+                    lstAlphabet.Items.Add(capital + "-> " + lowercase);
+
+                }
+
+            }
+
+
+        }
+    }
+}
