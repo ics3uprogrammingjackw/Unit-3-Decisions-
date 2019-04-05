495f9136b2770010ae0a4f0a99df2016dd822f36
JackWrightMac <jack.wright@ocsbstudent.ca>
2019-04-04 22:26:54 -0400

    Random number guess added

---
+++ NumGuessRanJackW/NumGuessJackW/NumGuessForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace NumGuessJackW
+{
+    partial class frmNumGuess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.picCheckmarkX = new System.Windows.Forms.PictureBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCheckmarkX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.lblQuestion.Location = new System.Drawing.Point(12, 52);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(187, 15);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "PIck a number between 1 and 10";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txtGuess.Location = new System.Drawing.Point(205, 49);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(71, 21);
+            this.txtGuess.TabIndex = 1;
+            this.txtGuess.TextChanged += new System.EventHandler(this.txtGuess_TextChanged);
+            // 
+            // picCheckmarkX
+            // 
+            this.picCheckmarkX.Image = global::NumGuessJackW.Properties.Resources.checkmark;
+            this.picCheckmarkX.Location = new System.Drawing.Point(293, 242);
+            this.picCheckmarkX.Name = "picCheckmarkX";
+            this.picCheckmarkX.Size = new System.Drawing.Size(131, 76);
+            this.picCheckmarkX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCheckmarkX.TabIndex = 2;
+            this.picCheckmarkX.TabStop = false;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(15, 154);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(116, 39);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(178, 155);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(133, 38);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // frmNumGuess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 348);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.picCheckmarkX);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "frmNumGuess";
+            this.Text = "Random Number Guess by Jack W";
+            this.Load += new System.EventHandler(this.frmNumGuess_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCheckmarkX)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.PictureBox picCheckmarkX;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
---
+++ NumGuessRanJackW/NumGuessJackW/NumGuessForm.cs
@@ -0,0 +1,111 @@
+﻿/*
+     * Created by: Jack Wright
+     * Created on: 27-2-19
+     * Created for: ICS3U Programming
+     * Daily Assignment – Day #17 - Random Number Guessing Game
+     * This program generates a random number than makes the user select a number  and tells them whether their guess was correct or incorrect.
+    */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace NumGuessJackW
+{
+    public partial class frmNumGuess : Form
+    {
+        //Declaring variables and constants
+        double guess;
+        Random randomNumberGenerator = new Random();
+        const int MIN_VALUE = 1;
+        const int MAX_VALUE = 10;
+        int correctNumber;
+
+        public frmNumGuess()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //Hides play button
+            btnPlay.Hide();
+
+            //Hides checkmark if player got it right the last play
+            picCheckmarkX.Hide();
+
+            //Clears the textbox from previous play
+            txtGuess.Text = "";
+
+            //Generates the random number
+            correctNumber = randomNumberGenerator.Next(MIN_VALUE, MAX_VALUE + 1);
+
+            //Enabling controls
+            btnCheck.Show();
+            lblQuestion.Show();
+            txtGuess.Show();
+
+
+        }
+
+        private void frmNumGuess_Load(object sender, EventArgs e)
+        {
+            //Disabling controls until play is clicked
+            picCheckmarkX.Hide();
+            btnCheck.Hide();
+            lblQuestion.Hide();
+            txtGuess.Hide();
+            btnPlay.Show();
+        }
+
+        private void txtGuess_TextChanged(object sender, EventArgs e)
+        {
+            //Hides checkmark/x
+            picCheckmarkX.Hide();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            //Gets user guess
+            guess = Convert.ToDouble(txtGuess.Text);
+
+            if (guess == correctNumber)
+            {
+                //Displays checkmark
+                this.picCheckmarkX.Image = Properties.Resources.checkmark;
+                picCheckmarkX.Show();
+
+                //Plays correct sound
+                SoundPlayer corrrect = new SoundPlayer(@"nice-work.wav");
+                corrrect.Play();
+
+                //Disables controls and displays play button
+                btnCheck.Hide();
+                lblQuestion.Hide();                
+                btnPlay.Show();
+            }
+
+            else
+            {
+                //Displays x
+                this.picCheckmarkX.Image = Properties.Resources.red_x;
+                picCheckmarkX.Show();
+
+                //Plays incorrect sound
+                SoundPlayer incorrrect = new SoundPlayer(@"maybe-next-time.wav");
+                incorrrect.Play();
+            }
+
+
+
+        }
+
+        
+    }
+}
